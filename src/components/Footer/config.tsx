eaae6d32468f3b7cbcf35572a13d92e5e341377a
Slava Pavlutin <sl.pavlutin@gmail.com>
2026-07-05 12:35:29 +0300

    initial footer

--- src/components/Footer/config.tsx
+++ src/components/Footer/config.tsx
@@ -1,10 +1,5 @@
 const config = {
-  sections: [
-    { title: 'Project', links: ['Houses', 'Rooms', 'Flats', 'Appartments'] },
-    { title: 'Company', links: ['How we work', 'Capital', 'Security'] },
-    { title: 'Movement', links: ['Who are we', 'Support us'] },
-    { title: 'Help', links: ['Privacy', 'Condition', 'Blog', 'FAQs'] },
-  ],
+  nav: ['Product', 'Pricing Plans', 'FAQ', 'Blog'],
 }
 
 export default config
--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,3 +1,33 @@
+import Logo from '~/assets/logo-mono.svg?react'
+import FacebookIcon from '~/assets/facebook-icon.svg?react'
+import InstagramIcon from '~/assets/instagram-icon.svg?react'
+import TwitterIcon from '~/assets/twitter-icon.svg?react'
+import YoutubeIcon from '~/assets/youtube-icon.svg?react'
+import config from './config'
+
 export const Footer = () => {
-  return <footer className=""></footer>
+  return (
+    <footer className="col-[wide] grid grid-cols-subgrid bg-blue-950 text-slatey-400">
+      <div className="col-[main] py-8 min-h-40 flex items-center justify-between gap-8">
+        <Logo className="w-32 md:w-45" />
+
+        <nav>
+          <ul className="flex items-center gap-4">
+            {config.nav.map((item) => (
+              <li key={item}>
+                <a href="#">{item}</a>
+              </li>
+            ))}
+          </ul>
+        </nav>
+
+        <div className="flex items-center gap-4">
+          <FacebookIcon />
+          <InstagramIcon />
+          <TwitterIcon />
+          <YoutubeIcon />
+        </div>
+      </div>
+    </footer>
+  )
 }
