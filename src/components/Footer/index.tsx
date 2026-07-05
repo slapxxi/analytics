107ff7e7340161b4f7f5ec061a8fa3fcccadf1cd
Slava Pavlutin <sl.pavlutin@gmail.com>
2026-07-05 18:29:03 +0300

    responsive pricing

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -8,7 +8,7 @@ import config from './config'
 export const Footer = () => {
   return (
     <footer className="row-6 z-10 col-[wide] grid grid-cols-subgrid text-xslate-400">
-      <div className="col-[main] flex min-h-40 items-center justify-between gap-8 py-8">
+      <div className="col-[main] border-t border-xslate-400/20 flex min-h-40 items-center justify-between gap-8 py-8">
         <Logo className="w-32" />
 
         <nav className="hidden text-sm md:block">
@@ -21,7 +21,7 @@ export const Footer = () => {
           </ul>
         </nav>
 
-        <div className="flex items-center gap-10">
+        <div className="flex items-center gap-6 md:gap-10">
           <FacebookIcon />
           <InstagramIcon />
           <TwitterIcon />
--- src/components/Pricing/index.tsx
+++ src/components/Pricing/index.tsx
@@ -7,7 +7,7 @@ export const Pricing = () => {
       <div className="hero col-[wide] row-[5/7] bg-xblue-900 clip-angle-rev" />
 
       <div className="z-10 col-[wide] row-5 grid grid-cols-subgrid py-10 pt-30 text-xslate-400">
-        <div className="col-[main] grid gap-20">
+        <div className="col-[wide] grid gap-20 md:col-[main]">
           <div className="mx-auto grid max-w-prose gap-7 text-center">
             <h2 className="title text-fluid-4xl text-white">Pricing Plans</h2>
             <p>
@@ -16,14 +16,20 @@ export const Pricing = () => {
             </p>
           </div>
 
-          <ul className="grid gap-4 py-10 md:grid-cols-3">
+          <ul
+            className="grid snap-x snap-mandatory 
+            grid-cols-[repeat(3,minmax(min(220px,70%),340px))] justify-between  gap-4
+            overflow-auto px-4 py-10 md:min-h-140"
+          >
             {config.plans.map((item, i) =>
               i === 1 ? (
                 <li
                   key={item.name}
-                  className="row-span-4 grid grid-rows-subgrid gap-4 rounded-xl bg-white p-4 text-center drop-shadow-lg drop-shadow-black/20 filter"
+                  className="row-span-4 grid snap-center grid-cols-[minmax(auto,220px)] grid-rows-subgrid 
+                  justify-center gap-10 rounded-3xl 
+                  bg-white p-4 text-center drop-shadow-lg drop-shadow-black/20 filter"
                 >
-                  <div>
+                  <div className="self-end">
                     <h3 className="title text-3xl text-black">{item.name}</h3>
                     <p className="text-fluid-base text-xslate-400">
                       {item.description}
@@ -31,14 +37,15 @@ export const Pricing = () => {
                   </div>
                   <hr className="border-xslate-400/30" />
                   <div className="text-center">{item.price}</div>
-                  <button className="btn">Order</button>
+                  <button className="btn self-start">Order</button>
                 </li>
               ) : (
                 <li
                   key={item.name}
-                  className="row-span-4 grid grid-rows-subgrid gap-4 rounded-xl bg-xblue-950 p-4 text-center"
+                  className="row-span-4 grid snap-center grid-cols-[minmax(auto,220px)] 
+                  grid-rows-subgrid justify-center gap-10 rounded-3xl bg-xblue-950 p-4 text-center"
                 >
-                  <div>
+                  <div className="self-end">
                     <h3 className="title justify-self-center text-3xl text-white">
                       {item.name}
                     </h3>
@@ -48,7 +55,7 @@ export const Pricing = () => {
                   </div>
                   <hr className="border-xslate-400/30" />
                   <div className="text-center">{item.price}</div>
-                  <button className="btn">Order</button>
+                  <button className="btn self-start">Order</button>
                 </li>
               ),
             )}
